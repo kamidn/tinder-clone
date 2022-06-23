a884cdb8b81553279f83ef4d3602b19fc3df8103
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-06-23 15:07:00 -0400

    Commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Header.css";
-//import PersonIcon from "@material-iu/icons/Person";
-import PersonIcon from '@mui/icons-material/Person';
+import PersonIcon from "@mui/icons-material/Person";
+import {IconButton} from "@mui/material";
+import ForumIcon from "@mui/icons-material/Forum";
 
 function Header() {
 	return (
 		<div className="header">
-			<h2>Header Area</h2>
+			<IconButton>
+				<PersonIcon fontSize="large" className="header__icon" />
+			</IconButton>
+			<img className="header__logo" src="./Tinder_logo.png" alt="" />
+			<IconButton>
+				<ForumIcon fontSize="large" className="header__icon" />
+			</IconButton>
 		</div>
 	);
 }

7967222409de94103dd1c7df5d6f7577cbf40ac6
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-06-24 17:07:28 -0400

    Commit

---
+++ src/Components/TinderCards.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./TinderCards.css";
+
+function TinderCards() {
+	const [people, setPeople] = useState([]) {
+		name: "Elon Musk",
+		url: ""
+	};
+
+	return
+		<div className="tinderCards">
+
+		</div>;
+}
+export default TinderCards;

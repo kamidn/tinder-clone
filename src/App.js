6956e1239c3cc25793e93048fb96bc7817cf4c15
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-06-25 15:38:11 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import TinderCards from "./Components/TinderCards";
+import SwipeButtons from "./Components/SwipeButtons";
 
 function App() {
   return (
     <div className="app">
-
 			<Header />
-			{/* TinderCards */}
-			{/* SwipeButtons */}
+			<TinderCards />
+			<SwipeButtons />
     </div>
   );
 }
--- src/Components/TinderCards.js
+++ src/Components/TinderCards.js
@@ -1,15 +1,56 @@
+import { Divider } from "@mui/material";
 import React, { useState } from "react";
+import TinderCard from "react-tinder-card"
 import "./TinderCards.css";
 
 function TinderCards() {
-	const [people, setPeople] = useState([]) {
-		name: "Elon Musk",
-		url: ""
+	const [people, setPeople] = useState([
+		{
+			name: "Pikachu",
+			url: "https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png"
+		},
+		{
+			name: "Bulbasaur",
+			url: "https://upload.wikimedia.org/wikipedia/en/2/28/Pok%C3%A9mon_Bulbasaur_art.png"
+		},
+		{
+			name: "Meowth",
+			url: "https://upload.wikimedia.org/wikipedia/en/a/aa/Pok%C3%A9mon_Meowth_art.png"
+		},
+	]);
+
+	const swiped = (direction, nameToDelete) => {
+		console.log("removing: " + nameToDelete);
 	};
 
-	return
-		<div className="tinderCards">
+	const outOfFrame = (name) => {
+		console.log(name + " left the screen");
+	};
 
-		</div>;
-}
-export default TinderCards;
+	return (
+		<div className="tinderCards">
+			<div className="tinderCards__cardContainer">
+				{
+					people.map((person) => (
+					<TinderCard
+						className="swipe"
+						key="person.name"
+						preventSwipe={["up", "down"]}
+						onSwipe={(dir) => swiped(dir, person.name)}
+						onCardLeftScreen={() => outOfFrame(person.name)}
+					>
+						<div
+							style={{ backgroundImage: `url(${person.url})` }}
+							className="card"
+						>
+							<h3>{person.name}</h3>
+						</div>	
+					</TinderCard>
+					
+				))};
+			</div>
+		</div>
+	);
+};
+
+export default TinderCards
